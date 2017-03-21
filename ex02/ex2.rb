b229a62f044d271ae7b09679fa10520d46c5f091
isabellarner <larner.isabel@gmail.com>
2017-03-21 11:36:38 +0000

    Exercise 2

---
+++ ex02/ex2.rb
@@ -0,0 +1,11 @@
+# This is a comment so you can read programs later.
+# Anything after the # is ignored
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can disable code with the #
+# puts "This won't run"
+
+puts "This will run."
+
+puts "This will # also run cos # is part of the string."
