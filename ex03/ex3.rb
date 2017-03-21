870d115a0e27ec2c3cccfc7e937e99a71eedd778
isabellarner <larner.isabel@gmail.com>
2017-03-21 15:39:25 +0000

    Exercise 3

---
+++ ex03/ex3.rb
@@ -0,0 +1,24 @@
+puts "I will now count my chickens:"
+#string
+puts "Hens #{25.00 + 30.00 / 6.00}"
+puts "Roosters #{100.00 - 25.00 * 3.00 % 4.00}"
+#prints strings with interpolated formulae
+puts "Now I will count the eggs:"
+#string
+puts 3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00
+#prints as a string the result of the formula
+puts "Is it true that 3 + 2 < 5 - 7?"
+#string
+puts 3.00 + 2.00 < 5.00 - 7.00
+#prints as a string the result of the formula as true or false (b/c <, > or == operators return true of false value)
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 -7? #{5 - 7}"
+#strings with interpolated formulae
+puts "Oh, that's why it's false."
+#string
+puts "How about some more."
+#string
+puts "Is it greater? #{5.00 > -2.00}"
+puts "Is it greater or equal? #{5.00 >= -2.00}"
+puts "Is it less or equal? #{5.00 <= -2.00}"
+#prints strings with result of interpolated formulae (as either true/false; remember < > or == operators return true or false value)
